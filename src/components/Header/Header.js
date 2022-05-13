09de6faa0c9d59adc8abe44e971c641b0937b5a7
2yunseong <dbsdltjd3701@naver.com>
2022-05-14 00:46:15 +0900

    feat : about, session 태그 추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,13 @@ import team_coffee_logo from "../img/team_coffee_logo.png";
 
 const Header = () => {
     return (
+        <>
         <img src={team_coffee_logo} alt="team coffe logo" />
+        <ul>
+            <li><a href="#about">about</a></li>
+            <li><a href="#session">session</a></li>
+        </ul>
+        </>
     );
 }
 
