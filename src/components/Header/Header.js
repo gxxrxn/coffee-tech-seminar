a7fe24096cc81c6c702f8b1c428884ff21bfbda5
2yunseong <dbsdltjd3701@naver.com>
2022-05-14 00:30:02 +0900

    feat : img 태그 추가

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import team_coffee_logo from "../img/team_coffee_logo.png";
+
+const Header = () => {
+    return (
+        <img src={team_coffee_logo} alt="team coffe logo" />
+    );
+}
+
+export default Header;
