231249b56f9e46bd84d737edf78197ad904e990f
2yunseong <dbsdltjd3701@naver.com>
2022-05-14 01:36:08 +0900

    feat : Header CSS 개선

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,20 @@
 import team_coffee_logo from "../img/team_coffee_logo.png";
 
+import "./Header.css";
+
 const Header = () => {
     return (
-        <>
-        <img src={team_coffee_logo} alt="team coffe logo" />
-        <ul>
-            <li><a href="#about">about</a></li>
-            <li><a href="#session">session</a></li>
+        <div className="header">
+        <img className="header-logo" src={team_coffee_logo} alt="team coffe logo" />
+        <ul className="header-link-list">
+            <li className="header-link-list-element">
+                <a className="header-link" href="#about">about</a>
+            </li>
+            <li className="header-link-list-element">
+                <a className="header-link" href="#session">session</a>
+            </li>
         </ul>
-        </>
+        </div>
     );
 }
 
