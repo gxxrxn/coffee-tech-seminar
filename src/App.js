6f583c3f4738810a0702e0267e5e04349252a113
2yunseong <dbsdltjd3701@naver.com>
2022-05-18 23:59:59 +0900

    chore : App Component로 Test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />  
+        <div id="about" className ="test1">
+          <h1>about</h1>
+        </div>
+        <div id="session" className ="test1">
+          <h1>session</h1>
+        </div>
       </header>
     </div>
   );
