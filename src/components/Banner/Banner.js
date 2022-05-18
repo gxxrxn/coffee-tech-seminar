b357521b71c63c389c258d2d7579ca2b0c72add6
2yunseong <dbsdltjd3701@naver.com>
2022-05-19 00:28:20 +0900

    feat : Banner 컴포넌트 기본 틀 구성

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,17 @@
+
+const Banner = () => {
+    return(
+        <div className="banner">
+            <div className="banner-text">
+                <h1>coffee tech<br/> seminar.</h1>
+                <div>front-end tech seminar by<br/> team coffee based in cnu econovation</div>
+                <h3><img alt="youtube link" /> 2022.06.21 pm 7</h3>
+            </div>
+            <div className="banner-img">
+
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
