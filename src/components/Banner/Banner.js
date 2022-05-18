c4be4fb5b5ab1239d9868fadc9ad23ed5f1da587
2yunseong <dbsdltjd3701@naver.com>
2022-05-19 00:52:39 +0900

    feat : banner 컴포넌트 이미지 추가

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,3 +1,11 @@
+import circle from "./img/circle.png";
+import code_icon from './img/code_icon.png';
+import youtube_logo from './img/youtube_logo.png';
+import arrow_down from './img/arrow_down.png';
+import scroll_down from './img/scroll_down.png';
+import hatch_circle from './img/hatch_circle.png';
+
+import "./Banner.css";
 
 const Banner = () => {
     return(
@@ -5,11 +13,13 @@ const Banner = () => {
             <div className="banner-text">
                 <h1>coffee tech<br/> seminar.</h1>
                 <div>front-end tech seminar by<br/> team coffee based in cnu econovation</div>
-                <h3><img alt="youtube link" /> 2022.06.21 pm 7</h3>
+                <h3><img alt="youtube link" src={youtube_logo}/> 2022.06.21 pm 7</h3>
             </div>
-            <div className="banner-img">
-
-            </div>
+            <img className="hatch-circle-img" alt="hatch-circle" src={hatch_circle} />
+            <img className="gray-circle-img" alt="circle" src={circle}/>
+            <img className="code-icon-img" alt="</>" src={code_icon} />
+            <img className="scroll-down-img" alt="scroll-down" src={scroll_down} />
+            <img className="arrow-down-img" alt="arrow-down" src={arrow_down} />
         </div>
     )
 }
