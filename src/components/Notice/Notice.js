0a2b05a290e30cb9682612a4814184415c3582c2
2yunseong <dbsdltjd3701@naver.com>
2022-05-24 00:53:20 +0900

    feat : Notice Component 생성

---
+++ src/components/Notice/Notice.js
@@ -0,0 +1,14 @@
+
+const Notice = () => {
+    return (
+        <div className="notice">
+            <h1>Notice</h1>
+            <div>team coffee의 소식이 궁금하다면?</div>
+            <div className="notice-button-box">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Notice;
