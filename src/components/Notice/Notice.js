b932d2d174d688e64d202a0e2e09553033e43fd0
2yunseong <dbsdltjd3701@naver.com>
2022-05-24 01:16:49 +0900

    feat : Notice 구성요소 추가

--- src/components/Notice/Notice.js
+++ src/components/Notice/Notice.js
@@ -1,11 +1,14 @@
+import './Notice.css';
 
 const Notice = () => {
     return (
         <div className="notice">
-            <h1>Notice</h1>
-            <div>team coffee의 소식이 궁금하다면?</div>
-            <div className="notice-button-box">
-                
+                <h1>Notice.</h1>
+                <div>team coffee의 소식이 궁금하다면?</div>
+            <div className="notice-links-box">
+                <a className="link-item" href="https://www.github.com/">github</a>
+                <a className="link-item" href="https://www.medium.com/">medium</a>
+                <a className="link-item" href="http://econovation.kr">econo</a>
             </div>
         </div>
     )
