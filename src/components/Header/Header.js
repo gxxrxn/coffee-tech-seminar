0bb87d59865e9a9b2fb0b4d0d8ef510d1c38ab5a
2yunseong <dbsdltjd3701@naver.com>
2022-05-31 02:28:43 +0900

    refactor : CSS 개선 - fade in 추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import team_coffee_logo from "../img/team_coffee_logo.png";
+import hamburger_icon from "./img/hamburger_icon.png";
 
 import "./Header.css";
 import NavList from "./NavList";
@@ -12,13 +13,19 @@ const Header = () => {
   };
   return (
     <div className='header'>
-      <img
-        className='header-logo'
-        src={team_coffee_logo}
-        alt='team coffe logo'
-      />
+      <a href='#' className='home-link'>
+        <img
+          className='header-logo'
+          src={team_coffee_logo}
+          alt='team coffe logo'
+        />
+      </a>
       <button className='hamburger-button' onClick={onClick}>
-        hamburger
+        <img
+          className='hamburger-button-img'
+          src={hamburger_icon}
+          alt='hamburger'
+        />
       </button>
 
       <NavList isNavVisable={isNavVisable} />
